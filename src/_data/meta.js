b91529c56c4dce0f9b28fd424be39486c2ed7ea0
wpickup <67346201+wpickup@users.noreply.github.com>
2024-05-23 20:55:27 +1000

    Change site name

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "Lee Pickup Ceramics",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, generated sitemap, and RSS feed.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "The web site of Lee Pickup Ceramics",
+  authorName: "Lee Pickup",
+  twitterUsername: "", // no `@`
 };
